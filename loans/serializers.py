a09f63e52b4077eeed734451e2825bdd6315fdce
Cleyviton <cleyvitonkeke@gmail.com>
2023-06-30 14:29:11 -0300

    feat: create Loans model

---
+++ loans/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Loan
+from copies.serializers import CopySerializer
+from users.serializers import UserSerializer
+
+
+class LoanSerializer(serializers.ModelSerializer):
+    copy = CopySerializer(read_only=True)
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Loan
+        fields = ["loan_date", "return_date", "copy", "user"]
+        extra_kwargs = {
+            "loan_date": {"read_only": True},
+            "return_date": {"read_only": True},
+        }
+
+        def create(self, validated_data: dict) -> Loan:
+            ...
