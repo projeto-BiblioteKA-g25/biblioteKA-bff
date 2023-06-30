baf5d0a89456738340eb5cacd86d3b0a612e7810
noelle <noelle.mtf@gmail.com>
2023-06-30 10:39:48 -0300

    feat: create user model

--- users/models.py
+++ users/models.py
@@ -3,4 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    ...
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=120, unique=True)
+    employee = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    block_end_date = models.DateTimeField(null=True, blank=True)
