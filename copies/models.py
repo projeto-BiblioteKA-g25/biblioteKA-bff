7ba158bea3862743f6a8cb50998e5c33717cd6a0
Cleyviton <cleyvitonkeke@gmail.com>
2023-07-03 10:41:27 -0300

    fix: fields

--- copies/models.py
+++ copies/models.py
@@ -5,11 +5,10 @@ class Copy(models.Model):
     class Meta:
         ordering = ["id"]
 
-    quantity = models.PositiveIntegerField(default=1)
     avaliable = models.BooleanField(default=True)
 
-    # book = models.ForeignKey(
-    #     "books.Book",
-    #     on_delete=models.CASCADE,
-    #     related_name="copies",
-    # )
+    book = models.ForeignKey(
+        "books.Book",
+        on_delete=models.CASCADE,
+        related_name="copies",
+    )
