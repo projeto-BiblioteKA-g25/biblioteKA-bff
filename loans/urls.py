6ac25bf2f1f69f7472b2c1337fd4f9c9aef1419c
Cleyviton <cleyvitonkeke@gmail.com>
2023-07-04 12:10:16 -0300

    feat: create view Loan

--- loans/urls.py
+++ loans/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import LoanView
+from .views import LoanView, LoanDatailsView
 
-urlpatterns = [path("loans/", LoanView.as_view())]
+urlpatterns = [
+    path("loans/", LoanView.as_view()),
+    path("loans/<int:pk>/", LoanDatailsView.as_view()),
+]
