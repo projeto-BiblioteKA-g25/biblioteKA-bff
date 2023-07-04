9ec9e6ca76a3890e06a9fe9b429372ff36888e08
Samuel-3004 <samuelszportela@hotmail.com>
2023-07-04 10:17:26 -0300

    feat: create permissions

--- users/serializers.py
+++ users/serializers.py
@@ -17,6 +17,7 @@ class UserSerializer(serializers.ModelSerializer):
             "block_end_date",
             "following",
         ]
+
         extra_kwargs = {
             "following": {"read_only": True},
             "password": {"write_only": True},
@@ -34,42 +35,6 @@ class UserSerializer(serializers.ModelSerializer):
                 ],
             },
         }
-        deth = 1
-
-    def create(self, validated_data: dict) -> User:
-        return User.objects.create_user(**validated_data)
-
-
-class UserBookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = [
-            "id",
-            "username",
-            "email",
-            "password",
-            "employee",
-            "is_active",
-            "block_end_date",
-            "following",
-        ]
-        extra_kwargs = {
-            "password": {"write_only": True},
-            "username": {
-                "validators": [
-                    UniqueValidator(
-                        queryset=User.objects.all(),
-                        message="A user with that username already exists.",
-                    )
-                ]
-            },
-            "email": {
-                "validators": [
-                    UniqueValidator(queryset=User.objects.all()),
-                ],
-            },
-        }
-        deth = 1
 
     def create(self, validated_data: dict) -> User:
         return User.objects.create_user(**validated_data)
